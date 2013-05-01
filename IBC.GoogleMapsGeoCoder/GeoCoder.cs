a76c698cad3ea74165ed6777ed2fa750571a6d9c
Jamie R. Rytlewski <jamier.net@gmail.com>
2013-05-01 12:17:39 -0400

    Initial Commit

---
+++ IBC.GoogleMapsGeoCoder/GeoCoder.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace IBC.GoogleMapsGeoCoder
+{
+    public class GeoCoder
+    {
+        public static Location Geocode(string address)
+        {
+            WebRequest request = WebRequest.Create("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=" + address);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            var results = JsonConvert.DeserializeObject<GoogleMapsGeocode>(responseFromServer);
+            reader.Close();
+            response.Close();
+            return results.results[0].geometry.location;
+        }
+        private class GoogleMapsGeocode
+        {
+            public Results[] results { get; set; }
+        }
+
+        private class Results
+        {
+            public Geometry geometry { get; set; }
+        }
+
+        private class Geometry
+        {
+            public Location location { get; set; }
+        }
+
+        public struct Location
+        {
+            public double lat { get; set; }
+            public double lng { get; set; }
+        }
+    }
+}
